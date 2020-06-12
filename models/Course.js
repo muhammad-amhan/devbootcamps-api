a32a444eb8d8c88ef1874201b51f7f12c897ba1e
Muhammad Amhan <muhammadamhan@gmail.com>
2020-06-12 15:39:24 +0100

    Added a middleware that calculates the average cost of courses for bootcamps

--- models/Course.js
+++ models/Course.js
@@ -40,4 +40,38 @@ const CourseSchema = new Schema({
     },
 });
 
+// Static method on the model to get the average of the course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    const average = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId },
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: { $avg: '$tuition' },
+            }
+        }
+    ]);
+    // Adding the averageCost to bootcamps
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(average[0].averageCost / 10) * 10,
+        })
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+// getAverageCost after saving the course information
+CourseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp).then(() => {});  // Suppressing the promise warning
+});
+
+// getAverageCost before remove
+CourseSchema.pre('remove', function () {
+    this.constructor.getAverageCost(this.bootcamp).catch(console.error); // Also suppresses the ignored promise warning
+});
+
+
 module.exports = mongoose.model('Course', CourseSchema);
